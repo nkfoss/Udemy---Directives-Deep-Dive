96963067074d781c69011dbca1f13dc69e8437f5
Nathan <nathankfoss@gmail.com>
2020-03-05 19:26:20 -0600

    Use HostBinding to change element style
    The renderer is no longer used.

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,6 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
+import { Directive, Renderer2,
+  OnInit, ElementRef,
+  HostListener, HostBinding } from '@angular/core';
 
 // This is a better practice than the implementation in basic-highlight.
 // Since service workers might not know about the DOM, basic-highlight might not
@@ -7,7 +9,12 @@ import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular
 @Directive({
   selector: '[appBetterHighlight]'
 })
-export class BetterHighlightDirective {
+export class BetterHighlightDirective implements OnInit{
+
+  // Here we have a property called 'backgroundColor, of type string.
+  // The host element of this our better-highlight directive has it's style.backgroundColor BOUND to our bgcolor string.
+  // Notice, that we when we mouseover the element, the string value changes. Hence, the style will also change.
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'green';
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
@@ -16,11 +23,13 @@ export class BetterHighlightDirective {
   }
 
   @HostListener('mouseenter') mouseOver(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'red');
+    this.backgroundColor = 'red'
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent')
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent'
   }
 
 
